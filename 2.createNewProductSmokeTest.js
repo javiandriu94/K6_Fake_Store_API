7579500a2aecd76d2528ec7d0d97bed15ad6ad59
Javier Vasquez C. <javiandriu21@gmail.com>
2025-03-23 23:59:04 -0500

    Fixing createNewProductSmokeTest script

--- 2.createNewProductSmokeTest.js
+++ 2.createNewProductSmokeTest.js
@@ -1,7 +1,7 @@
 import http from 'k6/http'
 import {check, sleep} from 'k6'
 import { Counter } from 'k6/metrics'
-import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { randomIntBetween, randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import { SharedArray } from 'k6/data';
 
 export const options = {
@@ -17,15 +17,17 @@ let httpError = new Counter('errorCounter')
 const productDataArray = new SharedArray('product information', function () {
     return JSON.parse(open('./fixture/product.json')).products;
 });
-export default function () {
+export default function createNewProduct() {
     const baseUrl = 'https://api.escuelajs.co/api/v1/';
     let product = { ...productDataArray[0] };
+    let newName = randomString(10)
     let newPrice = Math.floor(Math.random() * 100) + 1
     let newCategoryId = Math.floor(Math.random() * (5 - 1 + 1)) + 1
+    product.title = newName;
     product.price = newPrice;
     product.categoryId = newCategoryId;
         
-        let payload = JSON.stringify(product);
+    let payload = JSON.stringify(product);
         
         let params = {
             headers: {
@@ -37,24 +39,27 @@ export default function () {
             
         }
         let resCreateNewProduct = http.post(`${baseUrl}products`, payload, params);
-        
+        let newProductId = resCreateNewProduct.json().id
+        console.log(JSON.stringify(resCreateNewProduct.json()));
+
         if(resCreateNewProduct.error){
             httpError.add(1, {product: 'create'}),
             check(resCreateNewProduct, {
                 'isStatus400': r => r.status === 400,
             },{product: 'create'})
+            
         }else {
             check(resCreateNewProduct, {
-                'isStatus200': r => r.status === 200,
+                'isStatus200': r => r.status === 201,
                 'isIdAnInteger': r => typeof r.json().id === 'number',
                 'isTitleAString': r => typeof r.json().title === 'string',
                 'isPriceAnInteger': r => typeof r.json().price === 'number',
                 'isDescriptionAString': r => typeof r.json().description === 'string',
-                'DoesTheSlugExist': r => r.json().slug === r.json().title.toLowerCase(),
+                'DoesTheSlugExist': r => r.json().slug === r.json().title.toLowerCase().replace(/\s+/g, '-'),
                 'DoesCategoryExist': r => r.json().category.id === newCategoryId,
                 'categoryName': r => {
                 const category = r.json().category;
-                if (category.id === 1 && category.name === "Clothes" ) {
+                if (category.id === 1 && category.name === "Games" ) {
                     return true;
                 }else if (category.id === 2 && category.name === "Electronics") {
                     return true ;
@@ -73,5 +78,16 @@ export default function () {
                 {product: 'create'}
             )
         }
+    
         sleep(randomIntBetween(1,5)) 
+
+        if (resCreateNewProduct.status === 201) {
+            return newProductId;
+        } else {
+            return null;
+        }
+        
+        
+        
+          
 }
--- 3.getProductByIdSmokeTest.js
+++ 3.getProductByIdSmokeTest.js
@@ -1,4 +1,5 @@
 import http from 'k6/http'
+import createNewProduct from './2.createNewProductSmokeTest';
 import {check, sleep} from 'k6'
 import { Counter } from 'k6/metrics'
 import { randomIntBetween} from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
@@ -32,74 +33,20 @@ const productDataArray = new SharedArray('product information', function () {
 });
 export default function () {
     const baseUrl = 'https://api.escuelajs.co/api/v1/';
-    let product = { ...productDataArray[0] };
-    let newPrice = Math.floor(Math.random() * 100) + 1
-    let newCategoryId = Math.floor(Math.random() * (5 - 1 + 1)) + 1
-    product.price = newPrice;
-    product.categoryId = newCategoryId;
-        
-        let payload = JSON.stringify(product);
-        
-        let params = {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            tags: {
-                product:'create'
-            }
-            
+    let newProductId = createNewProduct ();
+    
+    let resGetProductById = http.get(`${baseUrl}products/` + newProductId, {
+        tags: {
+            product: 'productId'
         }
-        let resCreateNewProduct = http.post(`${baseUrl}products`, payload, params);
-        let newProductId = resCreateNewProduct.json().id;
-        
-        if(resCreateNewProduct.error){
-            httpError.add(1, {product: 'create'}),
-            check(resCreateNewProduct, {
-                'isStatus400': r => r.status === 400,
-            },{product: 'create'})
-        }else {
-            check(resCreateNewProduct, {
-                'isStatus200': r => r.status === 200,
-                'isIdAnInteger': r => typeof r.json().id === 'number',
-                'isTitleAString': r => typeof r.json().title === 'string',
-                'isPriceAnInteger': r => typeof r.json().price === 'number',
-                'isDescriptionAString': r => typeof r.json().description === 'string',
-                'DoesTheSlugExist': r => r.json().slug === r.json().title.toLowerCase(),
-                'DoesCategoryExist': r => r.json().category.id === newCategoryId,
-                'categoryName': r => {
-                const category = r.json().category;
-                if (category.id === 1 && category.name === "Clothes" ) {
-                    return true;
-                }else if (category.id === 2 && category.name === "Electronics") {
-                    return true ;
-                } else if (category.id === 3 && category.name === "Furniture") {
-                    return true;
-                }else if (category.id === 4 && category.name === "Shoes") {
-                    return true;
-                }else if (category.id === 5 && category.name === "Miscellaneous") {
-                    return true
-                }else {
-                    return false;
-                }
-                
-            }
-            },
-                {product: 'create'}
-            )
-        }
-        sleep(randomIntBetween(1,5));
+    });
 
-        let resGetProductById = http.get(`${baseUrl}products/` + newProductId, {
-            tags: {
-                product: 'productId'
-            }
-        });
-
-        if(resGetProductById.error){
-            httpError.add(1, {product: 'productId'}),
-            check(resGetProductById, {
-                'isStatus400': r => r.status === 400,
-            }, {product: 'productId'})
+    if(resGetProductById.error){
+        httpError.add(1, {product: 'productId'}),
+        check(resGetProductById, {
+            'isStatus400': r => r.status === 400,
+            'isErrorMessage': r => r.json().message === "Validation failed (numeric string is expected)"
+        }, {product: 'productId'})
         }else {
             check(resGetProductById, {
                 'isStatus200': r => r.status === 200,
